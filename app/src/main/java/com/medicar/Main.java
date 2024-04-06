be304935ab702ae08947598c9bbe801bb7806a27
Nahuel Rabey <nahuelrabeywork@gmail.com>
2024-04-06 15:36:56 -0300

    tested new classes

--- app/src/main/java/com/medicar/Main.java
+++ app/src/main/java/com/medicar/Main.java
@@ -1,13 +1,25 @@
 package com.medicar;
 
+import com.medicar.Medico.Medico;
 import com.medicar.Medico.MedicoSqlite;
+import com.medicar.Paciente.PacienteSqlite;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        MedicoSqlite.delete_table();
+
         MedicoSqlite.create_table_db();
-        System.out.println("Table created!");
+        System.out.println("medico Table created!");
         MedicoSqlite.new_medico("Rodrigo", "bc1123mpk");
-        System.out.println("Inserted into db!");
+        System.out.println("Inserted Rodrigo into db!");
+
+        PacienteSqlite.delete_table();
+        PacienteSqlite.create_table_db();
+        System.out.println("paciente Table created!");
+        PacienteSqlite.new_paciente("Jorge Macri", "25135410");
+        System.out.println("paciente Jorge Macri created!");
+
     }
 }
